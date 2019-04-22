e52abedc43d773b6686506d20800cf98e9b36517
Ivan <turlakov84@gmail.com>
2019-04-22 19:52:49 +0300

    tset2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const path = require('path');
 
 const app = express();
 
@@ -24,6 +25,16 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/bikes', require('./routes/api/bikes'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+  }
 
 const port = process.env.PORT || 5000;
 
